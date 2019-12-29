4361a7b8de10b79186fe43a9a5b5de59c1ce4be1
Ryo Ota <nwtgck@gmail.com>
2019-12-29 09:28:21 +0900

    Add missing "default" to the type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
 import { IAsyncComputedValue } from 'vue-async-computed';
 import { VueDecorator } from 'vue-class-component';
 
-export function AsyncComputed<T>(computedOptions?: IAsyncComputedValue<T>): VueDecorator;
+export default function AsyncComputed<T>(computedOptions?: IAsyncComputedValue<T>): VueDecorator;

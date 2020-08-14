95fde981a4fc9ef62e876aff42dca9b7c770a34e
Benjamin Fox <foxbenjaminfox@gmail.com>
2020-08-14 14:30:38 +0300

    Use the new IAsyncComputedValueBase from vue-async-computed as the options type
    Thi uses the IAsyncComputedValueBase type added in
    https://github.com/foxbenjaminfox/vue-async-computed/pull/91

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,7 @@
-import { IAsyncComputedValue } from "vue-async-computed";
+import { IAsyncComputedValue, IAsyncComputedValueBase } from "vue-async-computed";
 import { createDecorator, VueDecorator } from "vue-class-component";
 
-export interface IAsyncComputedOptions<TResult> {
-    // NOTE: We cannot reuse the IAsyncComputedValue<T> interface from
-    //       'vue-async-computed' because it requires the presence of a 'get'
-    //       property (which is obviously the decorated function in this case).
-
-    /**
-     * The default value or a function which return the default value that will
-     * be used until the data is loaded for the first time.
-     */
-    default?: TResult | (() => TResult);
-
-    /**
-     * An array of (nested) properties which are additional dependencies of
-     * this computed value.
-     */
-    watch?: string[] | (() => void);
-
-    /**
-     * A function which determines whether the recalculation of the value is
-     * required or not.
-     */
-    shouldUpdate?: () => boolean;
-
-    /**
-     * This flag indicates whether the value will be computed on first access
-     * or immediately when initializing the corresponding Vue component.
-     *
-     * By default, or if this flag is `false`, the former behavior will take
-     * place, otherwise (if the flag is `true`) the latter one will happen.
-     */
-    lazy?: boolean;
-}
+export type IAsyncComputedOptions<T> = IAsyncComputedValueBase<T>;
 
 export default function AsyncComputed<TResult>(
     computedOptions?: IAsyncComputedOptions<TResult>): VueDecorator {
